3ae8f65c2f737facfbdd98bef4f93d17e18d9942
Pedro <druskus20@gmail.com>
2022-05-20 02:32:16 +0200

    transforms and extensions to 11ty so it behaves like parcel would

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,31 @@
+const sass = require("sass");
+const ts = require("typescript");
+const posthtml = require("posthtml");
+const urls = require("posthtml-urls");
+
 module.exports = function(config) {
   config.addWatchTarget("./static/");
 
   config.addPassthroughCopy({
     static: "/"
   });
+    
+  addScssExtension(config);
+  addTsExtension(config);
+  addTransformScssLinks(config);
+  addTransformTsLinks(config);
+  addTransformIndexToHtmlLinks(config);
+  
+  config.addTemplateFormats(["md", "njk", "html"]);
 
   return {
-    templateFormats: ["md", "njk", "html"],
+    // templateFormats: ["md", "njk", "html"],
     // Pre-process *.md files with: (default: `liquid`)
     markdownTemplateEngine: "njk",
 
     // Pre-process *.html files with: (default: `liquid`)
     htmlTemplateEngine: "njk",
+
     dir: {
       input: "content",
       output: ".tmp_eleventy",
@@ -20,3 +34,94 @@ module.exports = function(config) {
     }
   };
 };
+
+function addTransformScssLinks(config) {
+  config.addTransform("transform-scsslinks", async function(contents) {
+    if (this.outputPath.endsWith(".html")) {
+      const plugin = urls({ eachURL: (url) => {
+        if (url.endsWith(".scss")) {
+          return url.slice(0, -5) + ".css";
+        }
+        return url;
+      }});
+      const res = (await (posthtml().use(plugin).process(contents))).html;
+      return res;
+    }
+    return contents; // no change done.
+  });
+}
+
+function addTransformTsLinks(config) {
+  config.addTransform("transform-tslinks", async function(contents) {
+    if (this.outputPath.endsWith(".html")) {
+      const plugin = urls({ eachURL: (url) => {
+        if (url.endsWith(".ts")) {
+          return url.slice(0, -3) + ".js";
+        }
+        return url;
+      }});
+      const res = (await (posthtml().use(plugin).process(contents))).html;
+      return res;
+    }
+    return contents; // no change done.
+  });
+}
+
+function addTransformIndexToHtmlLinks(config) {
+  config.addTransform("transform-htmllinks", async function(contents) {
+    if (this.outputPath.endsWith(".html")) {
+      // add "index.html" to links
+      const plugin = urls({ eachURL: (url) => {
+        // if its a directory
+        if (!(url.split('/').pop().indexOf('.') > -1)) {
+          if (url.charAt(url.length - 1) != "/") {
+            return url + "/index.html";
+          }
+          else {
+            return url + "index.html";
+          }
+        }
+        return url;
+      }});
+      const res = (await (posthtml().use(plugin).process(contents))).html;
+      return res;
+    }
+    return contents; // no change done.
+  });
+}
+
+function addScssExtension(config) {
+  config.addTemplateFormats("scss");
+  config.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: function (contents, inputPath) {
+      let includePaths = [this.config.dir.includes];
+      return () => {
+        let ret = sass.renderSync({
+          file: inputPath,
+          includePaths,
+          data: contents
+        });
+        return ret.css.toString("utf8");
+      };
+    }
+  });
+}
+
+function addTsExtension(config) {
+  config.addTemplateFormats("ts");
+  config.addExtension("ts", {
+    outputFileExtension: "js",
+    compile: function(source) {
+      return () => {
+        let ret = ts.transpileModule(source, {
+          compilerOptions: {
+            module: ts.ModuleKind.CommonJS
+          }
+        });
+        return ret.outputText;
+      };
+    }
+  });
+}
+
--- README.md
+++ README.md
@@ -4,15 +4,5 @@ A static website template project using 11ty and Parcel so I can reuse it for my
 
 ## TODO
 - [ ] More consistent naming scheme for directories
-- [ ] Try including the static content from root, without 11ty having to copy it to .tmp_eleventy. Instead parcel should just source it directly.
-  - this would mean, I would now run parcel from the root, not having to rely on hacky .hg and .parcelrc copy's
-  - might be able to remove the `static` watchtarget for 11ty, since parcel would do everything there.
-- [ ] might want to just do everything from 11ty since I would also like to compile the shortcodes/filters from ts to js
-  - This would mean that my ts and scss cant be directly linked 
-  - Might just use 11ty's global data then, and set the paths there  
-  - Setting custom handlers for ts / scss might be the thing https://github.com/11ty/eleventy/issues/117
-  - Might also want to inline css? Probably not.. is cached anyway, so this would just increase the page size
-- [ ] Currently `/index.html` is needed for parcel to source the files. We should try removing that in the end url after parcel bundles the project.
-- [ ] Make sure that PUBLIC_URL works when deploying on a server
 - [ ] Instead of using {{ Content }} maybe use blocks includes https://stackoverflow.com/questions/35496514/nunjucks-blocks-defined-within-include-partial-file-ignored-by-extend
 - [ ] Partials for footer / header etc
